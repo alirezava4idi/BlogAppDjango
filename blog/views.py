ee80d14f368f30001eb7fff8717d8b7565af930f
alirezava4idi <alireza.vahidi1@gmail.com>
2020-03-10 21:22:34 +0330

    Added html and css to home and about pages and made a base.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Alireza',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'May 16, 2020'
+    },
+    {
+        'author': 'Reza',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 17, 2020'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
