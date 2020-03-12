947e93d1fb4e23f492a998794a36e8cabb2749a5
alirezava4idi <alireza.vahidi1@gmail.com>
2020-03-12 20:00:07 +0330

    Added POST model.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Alireza',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'May 16, 2020'
-    },
-    {
-        'author': 'Reza',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 17, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
